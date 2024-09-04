79dc86401ba197b351a9e23d23d934b605106714
ch <mkmsacc@outlook.com>
2024-09-04 20:57:57 +0700

    Add initial Spring Boot application setup
    Introduced the main Spring Boot application structure, including `pom.xml`, a main application class, and necessary configuration files. Added Maven Wrapper to standardize project builds across different environments.

---
+++ src/main/java/io/github/chubbyhippo/echoserver/EchoserverApplication.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.echoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/chubbyhippo/echoserver/EchoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.echoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
